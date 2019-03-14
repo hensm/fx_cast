c8df4f985c5cfd7ac1818a7d17e52f4a8865f312
hensm <m+github@matt.tf>
2019-03-14 15:17:22 +0000

    Detect and offer platform-specific bridge download

--- docs/index.js
+++ docs/index.js
@@ -1,5 +1,47 @@
 "use strict";
 
+const downloadAppBtn = document.querySelector(".download__app");
+const downloadAppOther = document.querySelector(".download__app-other");
+const downloadAppOtherSummary = downloadAppOther.querySelector(":scope > summary");
+
+// Ext download button
+const downloadExtBtn = document.querySelector(".download__ext");
+
+// App download buttons
+const appListWinBtn = document.querySelector(".app-list__win");
+const appListMacBtn = document.querySelector(".app-list__mac");
+const appListDebBtn = document.querySelector(".app-list__deb");
+const appListRpmBtn = document.querySelector(".app-list__rpm");
+
+
+let platform;
+
+switch (navigator.platform) {
+    case "Win32":
+    case "Win64":
+        platform = "win";
+        downloadAppBtn.textContent = "Windows Bridge";
+        appListWinBtn.hidden = true;
+        break;
+
+    case "MacIntel":
+        platform = "mac";
+        downloadAppBtn.textContent = "macOS Bridge";
+        appListMacBtn.hidden = true;
+        break;
+
+    default:
+        /**
+         * Hide default download button and display other downloads
+         * without details summary.
+         */
+        downloadAppBtn.remove();
+        downloadAppOther.open = true;
+        downloadAppOtherSummary.hidden = true;
+}
+
+
+
 const ENDPOINT_URL = "https://api.github.com/repos/hensm/fx_cast/releases/latest";
 
 fetch(ENDPOINT_URL)
@@ -10,16 +52,6 @@ fetch(ENDPOINT_URL)
 function onResponse (res) {
     for (const asset of res.assets) {
         const { browser_download_url } = asset;
-
-        // Ext download button
-        const downloadExtBtn = document.querySelector(".download__ext");
-
-        // App download buttons
-        const appListWinBtn = document.querySelector(".app-list__win");
-        const appListMacBtn = document.querySelector(".app-list__mac");
-        const appListDebBtn = document.querySelector(".app-list__deb");
-        const appListRpmBtn = document.querySelector(".app-list__rpm");
-
 
         switch (asset.name.match(/.*\.(.*)$/).pop()) {
             case "xpi":
@@ -49,6 +81,24 @@ function onResponse (res) {
                 appListRpmBtn.removeAttribute("title");
                 break;
         }
+    }
+
+    if (platform) {
+        switch (platform) {
+            case "win":
+                downloadAppBtn.href = appListWinBtn.href;
+                break;
+            case "mac":
+                downloadAppBtn.href = appListMacBtn.href;
+                break;
+
+            default: {
+                return;
+            }
+        }
+
+        downloadAppBtn.removeAttribute("disabled");
+        downloadAppBtn.removeAttribute("title");
     }
 }
 
